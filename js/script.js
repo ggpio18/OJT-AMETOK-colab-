aaffbcdfa815d8f159233270c5ef0ea82f9eb713
louisemmv <louise.minton25@gmail.com>
2024-04-08 08:51:54 +0800

    upd experiences

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+const expNav = document.querySelectorAll(".experience__nav__link");
+const expContent = document.querySelectorAll(".experiences__item");
+expNav.forEach((exp) => {
+  exp.addEventListener("click", () => {
+    removeActiveExp();
+    exp.classList.add("active");
+    const activeContent = document.querySelector(`#${exp.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveExp() {
+  expNav.forEach((exp) => {
+    exp.classList.remove("active");
+  });
+}
+function removeActiveContent() {
+    expContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+  }
+
 const burgir = document.querySelector(".burger__menu");
 const headerA = document.querySelector(".header__action");
 burgir.addEventListener("click", () =>{
@@ -12,3 +35,4 @@ links.forEach((link) => {
     link.nextElementSibling.classList.toggle("open");
   });
 });
+
